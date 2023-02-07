ef1a4ef5eca07ef5ee9a972fdefbddce389d87b4
Morteza QorbanAlizade <gho.morteza@gmail.com>
2023-02-08 00:41:40 +0330

    like discussions

--- src/components/discussions/DiscussionItem.vue
+++ src/components/discussions/DiscussionItem.vue
@@ -16,7 +16,7 @@
       <div class="text">{{ data.text }}</div>
 
       <footer class="actions">
-        <span :class="`action-item likes ${data.iLikedIt ? 'liked' : ''}`">
+        <span :class="`action-item likes ${data.iLikedIt ? 'liked' : ''}`" @click="likeDislike">
           <b class="icon bi bi-hand-thumbs-up-fill"></b>
           <span class="count">{{ data.likes }}</span>
         </span>
@@ -24,7 +24,8 @@
       </footer>
 
       <div v-if="data.replies?.length" class="replyes">
-        <discussion-item v-for="reply in data.replies" :key="reply.id" :data="reply" :show-reply="false"></discussion-item>
+        <discussion-item v-for="reply in data.replies" :key="reply.id" :data="reply"
+          :show-reply="false"></discussion-item>
       </div>
 
     </div>
@@ -36,6 +37,11 @@
 
 import { defineProps } from "vue";
 import { timeStampToDate } from "../../functions.js"
+import { BASE_URL } from './../../Constants';
+import { useToast } from "vue-toast-notification";
+import axios from "axios";
+
+const toast = useToast();
 
 const props = defineProps({
   data: Object,
@@ -44,5 +50,18 @@ const props = defineProps({
     default: true
   }
 });
+
+const likeDislike = async () => {
+  try {
+    const dataModel = Object.assign(props.data, {
+      likes: props.data.iLikedIt ? props.data.likes -= 1 : props.data.likes += 1,
+      iLikedIt: !props.data.iLikedIt
+    });
+    console.log(dataModel)
+    const { status, data } = await axios.put(`${BASE_URL}/discussions/${props.data.id}`, dataModel);
+  } catch (err) {
+    toast.error(err.message)
+  }
+}
 
 </script>
